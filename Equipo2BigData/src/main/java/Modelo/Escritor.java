4c2a94873a407b84cfe26a940c7525c3f793cdc4
asierhernandezga <asier.hernandezga@elorrieta-errekamari.com>
2021-05-11 19:20:37 +0200

    Actualizacion de los dos informes y clase terminada

--- Equipo2BigData/src/main/java/Modelo/Escritor.java
+++ Equipo2BigData/src/main/java/Modelo/Escritor.java
@@ -1,15 +1,13 @@
 package Modelo;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class Escritor {
 
@@ -20,8 +18,13 @@ public class Escritor {
 
 			if (!Files.exists(path)) 		           
 				Files.createDirectory(path);
+			
+			DateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd.HHmmss");
+			Calendar cal = Calendar.getInstance();
+						
+			String DiaHora =dateFormat.format(cal.getTime());
 
-			FileWriter fich = new FileWriter("historico\\historico.csv");
+			FileWriter fich = new FileWriter("historico\\AlgoritmoNaiveBayesGeneral-"+DiaHora+".csv");
 			fich.write("Primer Producto,Segundo Producto,Fecha,Porcentaje");
 			for (int i=0;i<HistoricoGeneral.length;i++) {
 			fich.write("\n"+HistoricoGeneral[i][0]+","+HistoricoGeneral[i][1]+","+HistoricoGeneral[i][2]+","+HistoricoGeneral[i][3]);
@@ -36,5 +39,29 @@ public class Escritor {
 
 	public void escribirHistoricoLocal(String[][]HistoricoGeneral, String NIF) {
 		
+		try {
+			Path path = Paths.get("historico");
+
+			if (!Files.exists(path)) 		           
+				Files.createDirectory(path);
+			
+			DateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd.HHmmss");
+			Calendar cal = Calendar.getInstance();
+						
+			String DiaHora =dateFormat.format(cal.getTime());
+
+			FileWriter fich = new FileWriter("historico\\AlgoritmoNaiveBayesEspecifico-"+DiaHora+"-"+NIF+".csv");
+			fich.write("Primer Producto,Segundo Producto,Fecha,Porcentaje,NIF");
+			for (int i=0;i<HistoricoGeneral.length;i++) {
+			fich.write("\n"+HistoricoGeneral[i][0]+","+HistoricoGeneral[i][1]+","+HistoricoGeneral[i][2]+","+HistoricoGeneral[i][3]+","+NIF);
+			}
+			fich.close();
+
+		} catch (IOException ex) {
+			System.err.println("No se puede leer del archivo");
+			System.exit(-1);
+		}
 	}
+			
 }
+
